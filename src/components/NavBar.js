57d3a0d421d9557f23f4c0d18e09f97473f2778c
DEVENDRA MESHRAM <120394539+triggerdeva@users.noreply.github.com>
2023-03-16 01:58:22 +0530

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
-
-export const NavBar = () =>{
-    return(
-
-    )
-}
+export const NavBar = () => {
+	return (
+		<nav>
+			<ul>
+				<li>
+					<NavLink className={'index-link'} to={'/'}>
+						LOGO
+					</NavLink>
+				</li>
+				<li>
+					<NavLink className={'home-link'} to={'/home'}>
+						Home
+					</NavLink>
+				</li>
+			</ul>
+		</nav>
+	);
+};
